45eaba7b96665f0649d56dfa24e196e7dc731415
Stepanov Ilya <i.stepanov@jusan.kz>
2024-01-21 16:38:34 +0600

    Simple test updated

--- checkArticlePage.ts
+++ checkArticlePage.ts
@@ -1,8 +1,30 @@
-import { chromium } from 'playwright';
+import { chromium, Browser, Page, Response } from 'playwright';
 
 (async () => {
-    const browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0');
+    const browser: Browser = await chromium.launch({ headless: false });
+    const page: Page = await browser.newPage();
+
+    const urlToCheck: string = 'https://jusan.kz/business/osim/articles/kak-prinimat-mobilnye-platezhi-i-ne-narushat-zakon';
+
+    await page.goto(urlToCheck);
+
+    const links: string[] = await page.$$eval('a', (elements: HTMLAnchorElement[]) =>
+        elements.map((element) => element.href)
+    );
+
+    for (const link of links) {
+        const newPage: Page = await browser.newPage();
+        const response: Response | null = await newPage.goto(link, { waitUntil: 'domcontentloaded' });
+
+        // Проверяем HTTP-статус ответа, чтобы узнать, существует ли страница.
+        if (response && response.status() === 200) {
+            //console.log(`Ссылка ${link} рабочая.`);
+        } else {
+            console.error(`Ссылка ${link} не существует или возвращает ошибку.`);
+        }
+
+        await newPage.close();
+    }
+
     await browser.close();
 })();
