83336caf3694e4313830a46b27e94c6eb0b214df
Stepanov Ilya <i.stepanov@jusan.kz>
2024-01-23 15:55:05 +0600

    Исключены телефон и имейл

--- checkArticlePage.ts
+++ checkArticlePage.ts
@@ -1,10 +1,10 @@
 import { chromium, Browser, Page, Response } from 'playwright';
 
 (async () => {
-    const browser: Browser = await chromium.launch({ headless: false });
+    const browser: Browser = await chromium.launch({ headless: true });
     const page: Page = await browser.newPage();
 
-    const urlToCheck: string = 'https://jusan.kz/business/osim/articles/kak-prinimat-mobilnye-platezhi-i-ne-narushat-zakon';
+    const urlToCheck: string = 'https://test.jusan.kz:8476/business/kk/osim/articles/onlayn-tapsyrystardy-tabys-etu-punktin-ashu';
 
     await page.goto(urlToCheck);
 
@@ -13,18 +13,23 @@ import { chromium, Browser, Page, Response } from 'playwright';
     );
 
     for (const link of links) {
+        if (link === 'tel:88000800711' || link == 'tel:7711' || link == 'mailto:business@jusan.kz') {
+            continue;
+        }
+
         const newPage: Page = await browser.newPage();
         const response: Response | null = await newPage.goto(link, { waitUntil: 'domcontentloaded' });
 
         // Проверяем HTTP-статус ответа, чтобы узнать, существует ли страница.
         if (response && response.status() === 200) {
-            //console.log(`Ссылка ${link} рабочая.`);
+            console.log(`Ссылка ${link} рабочая.`);
         } else {
             console.error(`Ссылка ${link} не существует или возвращает ошибку.`);
         }
 
         await newPage.close();
     }
+
 
     await browser.close();
 })();
